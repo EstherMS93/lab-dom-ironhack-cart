96b80a3365025f7e61aa843703eb0dd2e979c280
EstherMS93 <esthermartinez@LAPTOP-BKUFCM5Q.localdomain>
2021-04-23 17:31:00 +0200

    done minu bonus. Iteration 3 still not working properly

--- js/index.js
+++ js/index.js
@@ -2,22 +2,32 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+const price = product.querySelector('.price span').innerText;
+const quantity = product.querySelector('.quantity input').value;
+const subtotal = price * quantity;
+product.querySelector('.subtotal span').innerText = subtotal.toFixed(2);
+return subtotal
 }
 
 function calculateAll() {
+
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+  
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  // ITERATION 2  // ITERATION 3
 
-  // ITERATION 3
-  //... your code goes here
+  const allProducts = document.getElementsByClassName('product');
+  const total = 0;
+  for (let i = 0; i < allProducts.length; i++) {
+    subtotal = updateSubtotal(allProducts[i]); 
+  }
+  document.querySelector("#total-value span").textContent = total.toFixed(2);
+ 
 }
 
 // ITERATION 4
